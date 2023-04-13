6715fdc5c4635005399be7ccbeb190b4c67d499e
Duy Huynh Quoc <duyhq.zee@gmail.com>
2023-04-13 14:32:29 +0700

    [New] Select a single unit

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField]
+    private UnityEvent onSelected = null;
+
+    [SerializeField]
+    private UnityEvent onDeselected = null;
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!isOwned)
+        {
+            return;
+        }
+
+        onSelected?.Invoke();
+    }
+
+    public void Deselect()
+    {
+        if (!isOwned)
+        {
+            return;
+        }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
