9161df56b87968f069c650582bf25541a3c2c850
Duy Huynh Quoc <duyhq.zee@gmail.com>
2023-04-14 16:28:20 +0700

    [New] Unit firing

---
+++ Assets/Scripts/Units/UnitFiring.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class UnitFiring : NetworkBehaviour
+{
+    [SerializeField]
+    private Targeter targeter = null;
+
+    [SerializeField]
+    private GameObject projectilePrefab = null;
+
+    [SerializeField]
+    private Transform projectileSpawnPoint = null;
+
+    [SerializeField]
+    private float fireRange = 15f;
+
+    [SerializeField]
+    private float fireRate = 1f;
+
+    [SerializeField]
+    private float rotationSpeed = 20f;
+
+    private float lastFireTime;
+
+    [ServerCallback]
+    private void Update()
+    {
+        Targetable target = targeter.GetTarget();
+
+        if (target == null)
+        {
+            return;
+        }
+
+        if (!CanFireAtTarget())
+        {
+            return;
+        }
+
+        if (Time.time > (1f / fireRate) + lastFireTime)
+        {
+            Debug.Log("Fire");
+
+            Quaternion targetRotation = Quaternion.LookRotation(
+                target.transform.position - transform.position
+            );
+
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
+
+            Quaternion projectTileRotation = Quaternion.LookRotation(
+                target.GetAimAtPoint().position - projectileSpawnPoint.position
+            );
+
+            GameObject projectileInstance = Instantiate(
+                projectilePrefab,
+                projectileSpawnPoint.position,
+                projectTileRotation
+            );
+
+            NetworkServer.Spawn(projectileInstance, connectionToClient);
+
+            lastFireTime = Time.time;
+        }
+    }
+
+    [Server]
+    private bool CanFireAtTarget()
+    {
+        Targetable target = targeter.GetTarget();
+
+        return (target.transform.position - transform.position).sqrMagnitude
+            <= fireRange * fireRange;
+    }
+}
